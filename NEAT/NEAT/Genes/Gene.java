5dd17e6995b684a786306f73753bfd205cc97324
Matthew Allen <AechPro@users.noreply.github.com>
2018-03-26 15:43:10 -0400

    Delete Gene.java

--- NEAT/NEAT/Genes/Gene.java
+++
@@ -1,8 +0,0 @@
-package NEAT.Genes;
-
-public abstract class Gene 
-{
-	private int innovationID;
-	public abstract void mutate(double[] probs);
-	public int getInnovationID() {return innovationID;}
-}
