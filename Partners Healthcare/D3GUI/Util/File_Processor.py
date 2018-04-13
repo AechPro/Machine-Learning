fcf96855bccf6a6dd0bfa290e62457168f5b7255
David Pacheco <dpacheco@mit.edu>
2018-04-13 12:47:00 -0400

    Added functionality to File_Processor.py

--- Partners Healthcare/D3GUI/Util/File_Processor.py
+++ Partners Healthcare/D3GUI/Util/File_Processor.py
@@ -4,36 +4,46 @@ from kivy.logger import Logger
 import sys
 from dropbox.files import WriteMode
 
-class File_Processor():
+
+class FileProcessor():
+    """
+    Class used to handle all file processing including syncing of local and remote directories, uploading, and downloading.
+    """
     def __init__(self, **kwargs):
         self._uploading = False
         self._downloading = False
-        self._dropbox = dropbox.Dropbox(kwargs['id'])
+        self._dbx = dropbox.Dropbox(kwargs['id'])
         self._remote_path = kwargs['remote_path']
         self._max_size = 1024*150
         self.local_path = kwargs['local_path']
+        self._flag = "{flag}"
 
-    def upload(self, file, name):
+    def upload(self, file):
+        """
+        :param file: file to be uploaded to the remote directory.
+        :return: void
+        """
         try:
             total_upload_size = os.path.getsize(file)
-            remote_path = self.local_path + name
+            remote_path = self.local_path + file
 
             with open(file, 'rb') as f:
                 if total_upload_size <= self._max_size:
-                    self._dropbox.files_upload(f.read(), path=remote_path, mode=WriteMode('overwrite'), mute=True)
+                    self._dbx.files_upload(f.read(), path=remote_path, mode=WriteMode('overwrite'), mute=True)
                 else:
                     try:
-                        upload_session_start_result = self._dropbox.files_upload_session_start(f.read(self._max_size))
+                        upload_session_start_result = self._dbx.files_upload_session_start(f.read(self._max_size))
                         cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
                                                offset=f.tell())
                         commit = dropbox.files.CommitInfo(path=remote_path)
 
                         while f.tell() < total_upload_size:
                             if (total_upload_size - f.tell()) <= self._max_size:
-                                self._dropbox.files_upload_session_finish(f.read(self._max_size), cursor, commit)
+                                self._dbx.files_upload_session_finish(f.read(self._max_size), cursor, commit)
                             else:
-                                self._dropbox.files_upload_session_append(f.read(self._max_size), cursor.session_id,  cursor.offset)
+                                self._dbx.files_upload_session_append_v2(f.read(self._max_size), cursor.session_id,  cursor.offset)
                     except dropbox.exceptions.ApiError as err:
+                        self.flag_file(file)
                         print('*** API error', err)
 
         except os.error:
@@ -43,6 +53,10 @@ class File_Processor():
             self._uploading = False
 
     def download(self):
+        """
+        Downloads a file from the remote directory into the local directory
+        :return:
+        """
         try:
             self._downloading = True
 
@@ -51,15 +65,15 @@ class File_Processor():
 
             # list the folder contents
             try:
-                res = self._dropbox.files_list_folder(self._remote_path)
+                res = self._dbx.files_list_folder(self._remote_path)
                 for entry in res.entries:
                     local_file = os.path.join(self.local_path, entry.name)
                     if not os.path.exists(local_file):
                         # download and overwrite it
-                        md, dl_res = self._dropbox.files_download(entry.path_lower)
+                        md, dl_res = self._dbx.files_download(entry.path_lower)
                         file = open(local_file, 'w')
                         file.write(dl_res.content)
-                        file.close
+                        file.close()
             except dropbox.exceptions.ApiError as err:
                 print('*** API error', err)
                 return None
@@ -69,5 +83,41 @@ class File_Processor():
         finally:
             self._downloading = False
 
+    def sync(self):
+        """
+        This function should find all the flagged files in the local directory, upload them to the remote directory, and deflag them
+        :return: void
+        """
 
-File_Processor(id="Yk7MLEza3NAAAAAAAAABGyzVVQi_3q7CkUoPjSjO6tWId31ogOM0KiBcdowZoB0b", remote_path="/input/D3RaspberryPi/", local_path='/home/pi/recon')
+        # look for flagged files
+        try:
+            for root, dirs, files in os.walk(self.local_path):
+                for file in files:
+                    if file.contains('{flag}'):
+                        self.upload(file)
+                        self.deflag(file)
+        except os.error:
+            e = sys.exec_info()[0]
+            Logger.exception('Exception! %s', e)
+
+    def flag(self, file):
+        """
+
+        :param file: file to be flagged locally, should only be flagged if there was an issue during an upload involving this file
+        :return: void
+        """
+        current_path_name = self._local_path+file
+        flagged_name = current_path_name[:-4]+self._flag+".txt"
+        os.rename(current_path_name, flagged_name)
+
+    def deflag(self, file):
+        """
+        :param file: file to be deflagged locally, should only be deflagged if a syncing operation successfully uploaded the file remotely
+        :return: void
+        """
+        current_path_name = self._local_path + file
+        deflagged_name = current_path_name[:-10] + ".txt"
+        os.rename(current_path_name, deflagged_name)
+
+
+FileProcessor(id="Yk7MLEza3NAAAAAAAAABGyzVVQi_3q7CkUoPjSjO6tWId31ogOM0KiBcdowZoB0b", remote_path="/input/D3RaspberryPi/", local_path='/home/pi/recon')
