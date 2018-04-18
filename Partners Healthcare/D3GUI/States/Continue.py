731aaffd9683f27ccce8773a3fb707756d89e1b1
David Pacheco <dpacheco@mit.edu>
2018-04-18 12:55:29 -0400

    Added Continue State

---
+++ Partners Healthcare/D3GUI/States/Continue.py
@@ -0,0 +1,21 @@
+from States import State
+from Display import Display as displays
+from Commands import Command as coms
+
+
+class Continue_State(State.State):
+    def execute(self):
+        super(Continue_State, self).execute()
+
+    #Refer to superclass documentation.
+    def _init_paths(self):
+        return
+
+    def _init_commands(self):
+        continue_same_user_command = coms.Continue_With_Same_User_Command(self)
+        continue_different_user_command = coms.Continue_With_Different_User_Command(self)
+        self._commands = {"CONTINUE WITH SAME\nUSER": continue_same_user_command,
+                          "CONTINUE WITH DIFFERENT\nUSER": continue_different_user_command}
+
+    def _build_display(self):
+        self._display = displays.Continue_Screen(self._commands, name="Continue Screen")
